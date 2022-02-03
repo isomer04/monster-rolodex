a0f4e4204d53a923cce4ee0664b253652cc73443
isomer04 <isomer008@gmail.com>
2022-02-02 20:39:09 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import './App.css';
-import { CardList} from './components/card-list/card-list.components';
-import React, {Component } from 'react';
-import './components/card-list/card-list.styles.css';
+import "./App.css";
+import { CardList } from "./components/card-list/card-list.components";
+import React, { Component } from "react";
+import "./components/card-list/card-list.styles.css";
 
-
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       monsters: [],
-      searchField: ''
+      searchField: "",
     };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ monsters : users }))
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
 
-  render () {
-
+  render() {
     // const monsters = this.state.monsters;
     // const  searchField = this.state.searchField;
-    // this shortcut and quickway writing 
+    // this shortcut and quickway writing
     const { monsters, searchField } = this.state;
-
-
-    const filteredMonsters =  monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
+    const filteredMonsters = monsters.filter((monster) =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
 
     return (
       <div className="App">
-        <input type='search' placeholder='search monsters' onChange={ e => {
-          this.setState({ searchField: e.target.value }, () => console.log(this.state));
-          
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
 
-        }} />
+          // { e => {
+          //   this.setState({ searchField: e.target.value }, () => console.log(this.state))}}
+        />
         <CardList monsters={filteredMonsters} />
-        
-    </div>
-    )
+      </div>
+    );
   }
 }
-
 
 export default App;
